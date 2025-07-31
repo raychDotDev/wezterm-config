31914d99bb29e4a609cb3bee9ff38dc16b74423b
raychDotDev <raychaser29@gmail.com>
2025-08-01 01:57:22 +0400

    thingies

--- wezterm.lua
+++ wezterm.lua
@@ -2,7 +2,24 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.default_prog = { "pwsh", "--nologo" }
+local launch_menu = {}
+
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	table.insert(launch_menu, {
+		label = "PowerShell",
+		args = { "pwsh.exe", "-nol" },
+	})
+	table.insert(launch_menu, {
+		label = "MSYS UCRT64",
+		args = { "cmd.exe ", "/k", "C:\\msys64\\msys2_shell.cmd -defterm -here -no-start -ucrt64 -shell bash" },
+	})
+	config.default_prog = { "cmd.exe ", "/k", "C:\\msys64\\msys2_shell.cmd -defterm -here -no-start -ucrt64 -shell bash" }
+elseif wezterm.target_triple == "x86_64-pc-linux" then
+	config.default_prog = { "bash" }
+end
+
+config.launch_menu = launch_menu
+
 
 config.font = wezterm.font('CaskaydiaMono Nerd Font')
 
@@ -12,6 +29,10 @@ if home == nil then
 	home = os.getenv("USERPROFILE");
 end
 
+config.keys = {
+	{ key = 'l', mods = 'ALT', action = wezterm.action.ShowLauncher },
+}
+
 config.background = {
 	{
 		source = {
@@ -20,23 +41,20 @@ config.background = {
 		repeat_x = 'NoRepeat',
 		width = 'Cover',
 		horizontal_align = 'Center',
-		height= 'Cover',
+		height = 'Cover',
 		vertical_align = 'Middle',
 		hsb = { brightness = 0.02 },
 	}
 }
 
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 
-config.initial_cols = 120
-config.initial_rows = 28
-
-config.font_size = 10
+config.font_size = 12
 config.color_scheme = 'Afterglow'
 
 config.use_fancy_tab_bar = false
@@ -44,34 +62,47 @@ config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
 
 config.colors = {
-  tab_bar = {
-    background = '#0b0b0b',
-    active_tab = {
-      bg_color = '#2b2b2b',
-      fg_color = '#c0c0c0',
-      intensity = 'Bold',
-      underline = 'None',
-      italic = true,
-    },
-    inactive_tab = {
-      bg_color = '#1b1b1b',
-      fg_color = '#808080',
-    },
-    inactive_tab_hover = {
-      bg_color = '#3b3b3b',
-      fg_color = '#909090',
-      italic = true,
-    },
-    new_tab = {
-      bg_color = '#1b1b1b',
-      fg_color = '#808080',
-    },
-    new_tab_hover = {
-      bg_color = '#3b3b3b',
-      fg_color = '#909090',
-      italic = true,
-    },
-  },
+	tab_bar = {
+		background = '#0b0b0b',
+		active_tab = {
+			bg_color = '#2b2b2b',
+			fg_color = '#c0c0c0',
+			intensity = 'Bold',
+			underline = 'None',
+			italic = true,
+		},
+		inactive_tab = {
+			bg_color = '#1b1b1b',
+			fg_color = '#808080',
+		},
+		inactive_tab_hover = {
+			bg_color = '#3b3b3b',
+			fg_color = '#909090',
+			italic = true,
+		},
+		new_tab = {
+			bg_color = '#1b1b1b',
+			fg_color = '#808080',
+		},
+		new_tab_hover = {
+			bg_color = '#3b3b3b',
+			fg_color = '#909090',
+			italic = true,
+		},
+	},
 }
+
+config.window_close_confirmation = 'NeverPrompt'
+table.insert(
+	config.keys,
+	{
+		key = 'w',
+		mods = 'SHIFT|CTRL',
+		action = wezterm.action.CloseCurrentTab { confirm = false },
+	}
+)
+wezterm.on('mux-is-process-stateful', function(_proc)
+	return false
+end)
 
 return config
